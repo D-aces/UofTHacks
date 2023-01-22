17d800da7b1fd61b4fe848fbd5bc28670dfea6fb
Dmi <84052843+DemiwizAces@users.noreply.github.com>
2023-01-22 05:05:49 -0500

    One of final commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,33 @@ import Navbar from './components/Navbar';
 import './App.css';
 import PersonForm from './components/PersonForm';
 import PlusIcon from './components/PlusIcon';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios, { Axios } from 'axios';
 
 function App() {
 
-  const [personForm, setPersonForm] = useState([<PersonForm key={0} />])
+  const[people, setPeople] = useState({})
+
+  const [personForm, setPersonForm] = useState([<PersonForm key={0} id={0} setPeople={setPeople}/>])
+
+  useEffect(() => {console.log(people)}, [people]);
 
   let renderNewForm = (e) => {
     e.preventDefault()
-    setPersonForm([...personForm, <PersonForm key={personForm.length} />])
+    setPersonForm([...personForm, <PersonForm key={personForm.length} id={personForm.length} setPeople={setPeople} />])
   }
 
+  const postFamily = () => {
+    console.log("Pressed")
+    axios.post('localhost:3000/success', 
+      people
+    )
+  }
 
   return (
     <div className="App">
       <Navbar />
-      <button className='submit-btn'>
+      <button className='submit-btn' onClick={postFamily}>
         <p>Generate Family Tree</p>
       </button>
       <h1>Add your family!</h1>
--- frontend/src/components/PersonForm.js
+++ frontend/src/components/PersonForm.js
@@ -1,43 +1,45 @@
 import React, { useEffect } from 'react'
 import FaceIcon from '@mui/icons-material/Face';
 import './PersonForm.css'
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
-function PersonForm() {
-  const [person, setPerson] = useState('')
-  const renderCount = useRef(0)
+function PersonForm(props) {
+  const id = props.id;
+  const setPeople = props.setPeople;
+
+  const [person, setPerson] = useState({})
 
   useEffect (() => {
-  renderCount.current = renderCount.current+1
-})
-
+    let newPeople = {};
+    newPeople[id] = person;
+    setPeople(p => ({...p, ...newPeople}));
+  }, [person, id, setPeople]);
 
 
   return (
     <div className='formContainer'>
       <div className='userImage'>
         <FaceIcon style={{ fill: "black", fontSize: 150 }}></FaceIcon>
-        <h4> {person ? 'This is ' + person : '???'}</h4>
+        <h4> {person.firstName ? 'This is ' + person.firstName : '???'}</h4>
       </div>
       <form className='htmlFormTemplate'>
         <fieldset>
           <div className='inputField'>
             <label htmlFor="firstName">First Name </label>
-            <input type="text" id="firstName" value={person} onChange={e => setPerson(e.target.value)}></input>
+            <input type="text" id="firstName" value={person.firstName} onChange={e => setPerson(p => ({...p, firstName: e.target.value}))}/>
           </div>
           <div className='inputField'>
             <label htmlFor="lastName">Last Name </label>
-            <input type="text" id="lastName"></input>
+            <input type="text" id="lastName" value={person.lastName} onChange={e => setPerson(p => ({...p, lastName: e.target.value}))}/>
           </div>
           <div className='inputField'>
             <label htmlFor="dateofbirth">Date of birth </label>
-            <input type="date" id="dateofbirth"></input>
+            <input type="date" id="dateofbirth" value={person.dob} onChange={e => setPerson(p => ({...p,dob: e.target.value}))}/>
           </div>
           <div className='inputField'>
             <label htmlFor="relationship">Type of relationship</label>
-            <select id="relationship" name="relationship">
-              <option disabled defaultValue> -- select an option -- </option>
-              <option value="father">Myself</option>
+            <select id="relationship" name="relationship"value={person.relationship} onChange={e => setPerson(p => ({...p,relationship: e.target.value}))}>
+              <option defaultValue>  </option>
               <option value="father">Father</option>
               <option value="mother">Mother</option>
               <option value="sibling">Sibling</option>
